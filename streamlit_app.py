7ef494128c46a1fd2133b997a60bfe89c16c8988
rafb59 <rafabelaval@gmail.com>
2025-07-07 04:09:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import plost
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
-with open('syle.css) as f:
+with open('syle.css') as f:
   st.markdown(f'<style>(f.read())</style>', unsafe_allow_html=True)
 
 st.sidebar.header('Dashboard `version 2`')
@@ -16,7 +16,7 @@ st.sidebar.subheader('Donut chart parameter')
 donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
 
 st.sidebar.subheder('Line chart parameters')
-plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min, 'temp_max'])
+plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
 plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
 
 st.sidebar.markdown('''
