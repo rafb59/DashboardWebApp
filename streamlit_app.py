64f9d39ac061c3486d90e1b0c5595f09d79fbe02
rafb59 <rafabelaval@gmail.com>
2025-07-07 05:10:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import plost
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
-with open('syle.css') as f:
+with open('style.css') as f:
   st.markdown(f'<style>(f.read())</style>', unsafe_allow_html=True)
 
 st.sidebar.header('Dashboard `version 2`')
